71332d6217632a1d81d78b36e0c7f0e78cf4078f
billdyu-lighthouse <1052931298@qq.com>
2022-10-25 20:27:34 +0800

    update

---
+++ sync/context_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCancelCtx(t *testing.T) {
+	p := context.TODO()
+	pCtx, pCancel := context.WithCancel(p)
+	ctx, cancel := context.WithCancel(pCtx)
+	go func(context.Context) {
+		time.Sleep(time.Second)
+		cancel()
+	}(ctx)
+
+	<-ctx.Done()
+	fmt.Println("child done: ", ctx.Err())
+	select {
+	case <-pCtx.Done():
+		fmt.Println("parent done: ", pCtx.Err())
+	default:
+		fmt.Println("parent not done") // 不影响父节点
+	}
+
+	pCancel() // 主动关闭父节点
+	fmt.Println("cancel parent")
+	select {
+	case <-pCtx.Done():
+		fmt.Println("parent done: ", pCtx.Err())
+	default:
+		fmt.Println("parent not done")
+	}
+}
